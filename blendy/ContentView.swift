4949647badb476fb4adf02ac42128e118aad8c1e
Seungmin Yang <tmdals099@gmail.com>
2022-11-14 00:20:30 +0900

    새 프로젝트 생성

---
+++ blendy/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  blendy
+//
+//  Created by 양승민 on 2022/11/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ blendy/blendyApp.swift
@@ -0,0 +1,17 @@
+//
+//  blendyApp.swift
+//  blendy
+//
+//  Created by 양승민 on 2022/11/14.
+//
+
+import SwiftUI
+
+@main
+struct blendyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
